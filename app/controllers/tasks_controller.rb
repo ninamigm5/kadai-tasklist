05483b89d0d5540353266d1c143aa4292fdb8409
masatoshi.goto <todo44ershishiggg@gmail.com>
2020-08-28 03:13:07 +0000

    user post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
+    #@user = User.find(params[:id])
+    #@tasks = @user.tasks.order(id: :desc).page(params[:page])
     @tasks = Task.all
   end
 
@@ -13,13 +17,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
+      
       render :new
     end
   end
@@ -47,6 +52,13 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
   # Strong Parameter
