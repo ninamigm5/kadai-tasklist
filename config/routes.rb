d766c20ebdec61173bdbed0d545109b721149670
masatoshi.goto <todo44ershishiggg@gmail.com>
2020-08-29 01:22:36 +0000

    Fix5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
-  
   root to: 'tasks#index'
-
   resources :tasks
 
   get 'login', to: 'sessions#new'
@@ -14,6 +7,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:create]
+  #resources :users, only: [:index, :show, :create]
   
 end
